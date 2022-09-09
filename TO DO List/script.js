e6f970d462be042725106748e3a63b604f63cc3f
Phoenix <anmolkhanna2003@gmail.com>
2022-09-09 10:42:52 +0530

    random updates Revert "To Do list Part1"
    This reverts commit 1ca38c973ab39d2b2446f11d15b0d1e4a090d824.

---
+++ TO DO List/script.js
@@ -0,0 +1,59 @@
+
+function AddTask(value){
+    const li=document.createElement("li");
+    li.classList.add("notDone");
+    li.textContent=value;
+    const ul=document.querySelector('ul');
+    console.log(23);
+    ul.append(li);    
+}
+//const EntryValue=document.querySelector("input");
+//let text=EntryValue.value;
+const buttons=document.querySelectorAll(".two");
+buttons.forEach(element=>{
+    //add a EL here
+    element.addEventListener("click",function(e){
+        if(e.target.textContent==="Add"){
+            const EntryValue=document.querySelector("input");
+            let text=EntryValue.value;
+            if(text==""){
+                console.log("no text");
+            }else{
+                AddTask(text);
+                DoneOnes();
+                EntryValue.value=null;
+                
+            }
+        }else{
+            Remove();
+        
+        }
+    })
+})
+function Remove(){
+    const all=document.querySelectorAll("li");
+    console.log(all);
+    all.forEach(e=>{
+        let test=e.getAttribute('class');
+        if(test.includes('done')){
+            e.remove();
+            return true;
+        }
+        
+    })
+}
+function DoneOnes(){
+    const all=document.querySelectorAll("li");
+    console.log(all);
+    all.forEach(o=>{
+        o.addEventListener("click",(e)=>{
+            const select=e.target;
+            if(select.tagName=="LI"){
+                select.style.textDecoration="line-through";
+                select.classList.add("done")
+            
+            }
+        })
+    })
+}
+=
